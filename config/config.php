c3a36b0f4b8a600017c965ceaa0b87901a654a08
isaac <isaacghorbani@gmail.com>
2018-11-26 12:50:18 +0330

    =set config keys.

--- config/config.php
+++ config/config.php
@@ -1,6 +1,15 @@
 <?php
 
 return [
-
+    'sms_username'=>'username',
+    'sms_password'=>'password',
+    'numbers'=>[
+        'ads_1'=>'00985000145',
+        'ads_2'=>'00985000949',
+        'sim'=>'0098sim',
+        'main'=>'0098100009',
+    ],
+    "registered_msg"=>"Register Message",
+    "reset_password_request"=>"Reset_Password Message",
 
 ];
